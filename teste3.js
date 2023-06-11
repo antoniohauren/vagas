4ca12c59b3be6fe79804dd04b1339f69655228c3
Antonio Hauren <antoniohauren@protonmail.com>
2023-06-11 11:19:20 -0300

    fix: delete by id

--- teste3.js
+++ teste3.js
@@ -1,40 +1,40 @@
 const data = require("./fakeData");
 
 const deleteByName = (req, res) => {
-    const name = req.query.name;
+  const name = req.query.name;
 
-    let userFound = null;
+  let userFound = null;
 
-    if (name) {
-        userFound = data.findIndex((user) => user?.name === name);
-    }
+  if (name) {
+    userFound = data.findIndex((user) => user?.name === name);
+  }
 
-    if (userFound !== -1) {
-        data[userFound] = null;
-        return res.send({ data: "Success" });
-    }
+  if (userFound !== -1) {
+    data[userFound] = null;
+    return res.send({ data: "Success" });
+  }
 
-    res.send({ error: "User not found" });
+  res.status(404).send({ error: "User not found" });
 };
 
 const deleteById = (req, res) => {
-    const id = req.query.id;
+  const id = req.params.id;
 
-    let userFound = null;
+  let userFound = null;
 
-    if (id) {
-        userFound = data.findIndex((user) => user.id === id);
-    }
+  if (id) {
+    userFound = data.findIndex((user) => user.id === Number(id));
+  }
 
-    if (userFound && userFound !== -1) {
-        data[userFound] = null;
-        return res.send({ data: "Success" });
-    }
+  if (userFound !== null && userFound !== -1) {
+    data[userFound] = null;
+    return res.send({ data: "Success" });
+  }
 
-    res.send({ error: "User not found" });
+  res.status(404).send({ error: "User not found" });
 };
 
 module.exports = {
-    deleteById,
-    deleteByName,
-}
+  deleteById,
+  deleteByName,
+};
